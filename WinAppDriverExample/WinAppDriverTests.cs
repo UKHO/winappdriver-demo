a6e4c1a137764d81d53df27586a204631f274185
Sara Gowen <sara.gowen@ukho.gov.uk>
2020-06-08 14:25:53 +0100

    moved environment specific settings to config file

--- WinAppDriverExample/WinAppDriverTests.cs
+++ WinAppDriverExample/WinAppDriverTests.cs
@@ -1,16 +1,27 @@
 using System;
+using Microsoft.Extensions.Configuration;
 using NUnit.Framework;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Windows;
 
 namespace WinAppDriverExample
 {
+    [TestFixture]
     public class WinAppDriverTests
     {
         private WindowsDriver<WindowsElement> _winDriver;
+        private IConfigurationRoot _config;
+
+        [OneTimeSetUp]
+        public void SetUpAppConfig()
+        {
+            _config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
 
         [SetUp]
-        public void SetUp()
+        public void SetUpAppiumAndWinAppDriver()
         {
             var options = new AppiumOptions();
             options.AddAdditionalCapability("deviceName", "WindowsPC");
@@ -18,9 +29,9 @@ namespace WinAppDriverExample
             //If you already have the process open then use the below:
             //    options.AddAdditionalCapability("appTopLevelWindow", topLevelWindowHandle);
             //Otherwise use this to launch the app:
-            options.AddAdditionalCapability("app", GetPathToApplicationUnderTest());
+            options.AddAdditionalCapability("app", _config["PathToApplicationUnderTest"]);
 
-             _winDriver =  new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options);
+             _winDriver =  new WindowsDriver<WindowsElement>(new Uri(_config["WinAppDriverUrl"]), options);
         }
 
         [TearDown]
@@ -38,18 +49,13 @@ namespace WinAppDriverExample
 
             //you can use class name to find an element but try to avoid this when possible
             var button = _winDriver.FindElementByClassName("Button");
-            Assert.That(button.Text, Is.EqualTo("Press me"), "Button text is incorrect");
+            Assert.That(button.Text, Is.EqualTo("Press me!"), "Button text is incorrect");
             button.Click();
 
             //you can also use FindElementByAccessibilityId for x:Name.
             //if AutomationProperties.AutomationId is also on the element then FindElementByAccessibilityId will expect that value instead
             var result = _winDriver.FindElementByAccessibilityId("ResultWindow");
             Assert.That(result.Text, Is.EqualTo("Input was stuff"), "Result text is incorrect");
-        }
-
-        private static string GetPathToApplicationUnderTest()
-        {
-            return @"C:\Dev\WinAppDriverExample\TestApp\bin\Debug\netcoreapp3.1\TestApp.exe";
         }
     }
 }
